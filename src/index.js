3fc2066ce7233e1577571fa850514f89c9c6b6cf
Jittymol Mathew <mathewjittymol@gmail.com>
2025-11-05 23:54:25 +0100

    Fixed 2 challenges with Performance issue and context extension

--- src/index.js
+++ src/index.js
@@ -7,25 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const USER_ROLE_PATH = './userRole.json';
-const userRoles = fs.existsSync(USER_ROLE_PATH) 
-  ? JSON.parse(fs.readFileSync(USER_ROLE_PATH, 'utf-8'))
-  : {"system": "System"};
-
-function getRoleContext(userPrompt) {
-  // Simple role detection (can be improved with NLP later)
-  const prompt = userPrompt.length > 0 ? userPrompt[0]?.content?.toLowerCase() : '';
-
-  if (prompt.includes("as a business analyst") || prompt.includes("business analyst")) return userRoles.ba;
-  if (prompt.includes("as an architect") || prompt.includes("architect")) return userRoles.architect;
-  if (prompt.includes("as a designer") || prompt.includes("designer")) return userRoles.designer;
-  if (prompt.includes("as a developer") || prompt.includes("developer")) return userRoles.developer;
-
-  // default to mixed role
-  return userRoles.mixed;
-}
-
-
 //Context Json Import
 const CONTEXT_PATH = './context.json';
 const context = fs.existsSync(CONTEXT_PATH)
@@ -36,16 +17,15 @@ const context = fs.existsSync(CONTEXT_PATH)
 app.post('/api/generate', async (req, res) => {
   try {
     const { messages } = req.body;
-    const userRoleVal = getRoleContext(messages);
     const promptWithContext = [
-      { role: 'system', content: context },
+      { role: 'user', content: context },
       ...messages,
     ];
     const ollamaRes = await fetch('http://localhost:11434/api/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        model: 'Kris',
+        model: 'gemma3:1b',
         messages: promptWithContext,
         // stream: false,
         stream: true,
